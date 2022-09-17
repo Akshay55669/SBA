a24207a5790205631d31b6920e0503943b73fec1
Bhanubediya <bhanubediya@gmail.com>
2022-09-17 18:53:09 +0530

    components and home UI added

--- sbaUI/src/app/app-routing.module.ts
+++ sbaUI/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
 
 const routes: Routes = [
+  //BHANU -- 17/09/22 -- Adding paths
+  {path:'',component:HomeComponent},
+  {path:'register',component:RegisterComponent},
   {path:'login',component:LoginComponent}
 ];
 
--- sbaUI/src/app/app.module.ts
+++ sbaUI/src/app/app.module.ts
@@ -1,33 +1,30 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import{HttpClientModule}from "@angular/common/http"
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
 import { RegisterComponent } from './Components/register/register.component';
 import { LoginComponent } from './Components/login/login.component';
-
-
+import { HomeComponent } from './Components/home/home.component';
+import { HeaderComponent } from './Components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-   
-  
-        RegisterComponent,
-        LoginComponent
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
